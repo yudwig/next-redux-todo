7421ab0da64eb1436411e140712f0c2de2ea0131
yudwig <yudwig.dev@gmail.com>
2021-04-27 21:56:26 +0900

    Fix config files.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,4 @@
 module.exports = {
-  stories: [
-    "../src/stories/**/*.stories.mdx",
-    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
 };
