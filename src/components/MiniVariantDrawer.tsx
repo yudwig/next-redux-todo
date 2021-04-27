cc0da0913c4b9ac579ad4c7c6950804f10b402cb
yudwig <yudwig.dev@gmail.com>
2021-04-27 09:41:06 +0900

    Refactor codes.

--- src/components/MiniVariantDrawer.tsx
+++ src/components/MiniVariantDrawer.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import {Drawer, DrawerProps} from "@material-ui/core";
+import { Drawer, DrawerProps } from "@material-ui/core";
 
-const MiniVariantDrawer: React.FC<DrawerProps> = ({children, ...props}) => {
+const MiniVariantDrawer: React.FC<DrawerProps> = ({ children, ...props }) => {
   return (
     <Drawer {...props}>
       <span>Mini Variant Drawer</span>
-      { children }
+      {children}
     </Drawer>
   );
-}
+};
 export default MiniVariantDrawer;
