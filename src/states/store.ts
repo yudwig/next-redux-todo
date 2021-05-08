b27ecbfb748a4b6b8925e9ae27ba5f486a71d2de
yudwig <yudwig.dev@gmail.com>
2021-05-08 22:05:35 +0900

    Add redux devtools.

--- src/states/store.ts
+++ src/states/store.ts
@@ -1,7 +1,10 @@
 import { combineReducers, createStore } from "redux";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { composeWithDevTools } from "redux-devtools-extension";
 import tasksReducer from "./tasks/reducers";
 
 export default function configureStore(states = {}) {
   const reducers = combineReducers({ tasksReducer });
-  return createStore(reducers, states);
+  const composedEnhancers = composeWithDevTools();
+  return createStore(reducers, states, composedEnhancers);
 }
