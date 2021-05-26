c289aa685a3031354c32633c0d312001f8952616
yudwig <yudwig.dev@gmail.com>
2021-05-26 09:11:24 +0900

    Fix icon design.

--- src/components/atoms/ArchivedStateButton/ArchivedStateButton.tsx
+++ src/components/atoms/ArchivedStateButton/ArchivedStateButton.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
 import { ButtonProps, IconButton } from "@material-ui/core";
 import styled from "styled-components";
-import UnarchiveIcon from "@material-ui/icons/Unarchive";
+import UnarchiveOutlinedIcon from "@material-ui/icons/UnarchiveOutlined";
 
 const Button = styled(IconButton)`
   padding: 9px;
@@ -15,7 +15,11 @@ interface Props extends ButtonProps {
 const ArchivedStateButton: React.FC<Props> = (props) => {
   return (
     <Button aria-label="delete">
-      {props.iconType === "archive" ? <DeleteOutlineIcon /> : <UnarchiveIcon />}
+      {props.iconType === "archive" ? (
+        <DeleteOutlineIcon />
+      ) : (
+        <UnarchiveOutlinedIcon />
+      )}
     </Button>
   );
 };
