62fe448e972b3eb6763862e8b2f3046f6e32314b
yudwig <yudwig.dev@gmail.com>
2021-05-28 19:15:06 +0900

    Add decorator to storybook.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,10 @@
+import { MuiThemeProvider, StylesProvider } from "@material-ui/core";
+import theme from "../src/configs/theme";
+import { ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
+import store from "../src/states/store";
+import React from "react";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +14,17 @@ export const parameters = {
     },
   },
 };
+
+export const decorators = [
+  (Story) => (
+    <StylesProvider injectFirst>
+      <MuiThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
+          <Provider store={store}>
+            <Story />
+          </Provider>
+        </ThemeProvider>
+      </MuiThemeProvider>
+    </StylesProvider>
+  ),
+];
